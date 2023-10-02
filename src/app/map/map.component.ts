b32f7bafdd3c9060de54e33015090a3d5288f80f
Revenant70 <jackmcg777@protonmail.com>
2023-10-02 13:58:07 -0400

    Add: Created click handler for svg map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent {
 
+  clickHandler(e:Event){
+
+    /*Gets if from SVG path*/
+    const id = e.target as HTMLElement
+    console.log(id.getAttribute('id'))
+
+  }
+
+  
 }
